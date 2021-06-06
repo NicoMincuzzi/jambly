3d52784253a811b6c882568b1c3a33aa17ea1aee
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-06-07 00:19:17 +0200

    [refactor]: Rename variables

--- src/main/java/com/lefc/jambly/ErrorParser.java
+++ src/main/java/com/lefc/jambly/ErrorParser.java
@@ -6,13 +6,13 @@ import java.util.Iterator;
 
 public class ErrorParser {
 
-    public void print_error(ArrayList<PrintText> error, int cont_errori) throws IOException {
-        Iterator<PrintText> err_list = error.iterator();
+    public void print_error(ArrayList<PrintText> error, int errorCounter) throws IOException {
+        Iterator<PrintText> iterator = error.iterator();
 
-        Support.writeFileErr("ERRORI TROVATI: " + cont_errori + "\n");
-        Support.countExit(cont_errori);
-        while (err_list.hasNext()) {
-            PrintText stampa = err_list.next();
+        Support.writeFileErr("ERRORI TROVATI: " + errorCounter + "\n");
+        Support.countExit(errorCounter);
+        while (iterator.hasNext()) {
+            PrintText stampa = iterator.next();
             Support.writeFileErr("\n" + stampa.getMessage() + "OUTPUT ERROR:\n" + stampa.getString() + "\nRiga n." + stampa.getPos() + "\n");
         }
     }
@@ -26,41 +26,4 @@ public class ErrorParser {
         output.setMessage(message);
         error.add(output);
     }
-
-    //chiamata in auto da syntax_error e fatal_error, per il controllo era necessario fare un override
-    //crea oggetti di tipo PrintText
-    /*public void report_error(String message, Object info) {
-        cont_errori++; //incremento degli errori
-        int pos = scan.getPos_vir();
-
-        //il null serve per la syntax_error e per la fatal_error
-        if (info == null) {
-            try {
-                output = scan.text(); //ottengo pt della funzione text()
-
-                if (!fatal) {
-                    output.setMessage(message);
-                    error.add(output);
-                } else { //caso in cui viene superata la fine del file
-                    output.setMessage(message);
-                    if (output.getPos() >= pos)  //getPos() posizione riferita all'oggetto errato
-                        output.setString("");
-                    fatal = false;
-                    error.add(output);
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        } else {
-            if (info instanceof Obj) {
-                try {
-                    output = scan.text();
-                    output.setMessage(message);
-                    error.add(output);
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }
-    }*/
 }
